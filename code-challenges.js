353362a9e2d63dd75121f69b9325bfdab5e65d12
kabowser <k.a.bowser32@gmail.com>
2022-10-30 18:57:48 -0700

    finished

--- code-challenges.js
+++ code-challenges.js
@@ -20,9 +20,15 @@ const fruit4 = "kiwi"
 
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
-// Describe your process:
+// function whether or not temperature is below above or at boiling point.
+//input: number
+//output: boiling, not boiling, or above boiling
+//conditional: if 
+// return
+// Describe your process:  Kept having issues with parameters and console.log until i realized i wasnt actually putting in number value in the log.
 
 const temp1 = 42
+
 // Expected output: "42 is below boiling point"
 
 const temp2 = 350
@@ -31,38 +37,82 @@ const temp2 = 350
 const temp3 = 212
 // Expected output: "212 is at boiling point"
 
+const waterTemp = (temp) => {
+    if (temp < 212){
+    return "42 is bellow boiling point"
+  } else if(temp > 212) {
+    return "350 is above boiling point"
+  } else if (temp === 212) {
+    return "212 is at boiling point"
+  } else {
+    return "error"
+  }
+}
+console.log(waterTemp(42))
+console.log(waterTemp(350))
+console.log(waterTemp(212))
+
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Describe your process:
+//input: two arrays
+//output:length of combined arrays
+// Need to find built in method to combine arrays and then
+//return number of values in array.
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+
+console.log(padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns))
+var padresGames = console.log(padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns))
+console.log(padresGames)
+
+
 // Expected output: 9
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Describe your process:
-
+//reverse built in method to reverse the string.
+// Attempted to convert in to an array so that i could use the built in reverse method. Unsuccessful.
 const currentCohort = "Golf 2022"
+
+console.log(currentCohort.split())
+var reverseCohort = console.log(currentCohort.split())
+console.log(reverseCohort.reverse())
 // Expected output: "2202 floG"
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Describe your process:
-
+//Tried to solve code with mutators and accessors. After looking through lectures realized i could identify the specific index with a loop. 
+//Only want value of 42 so start and stop at 7th index.
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 const givenValue1 = 42
+for (let i=7; i===7; i++)
+console.log(i)
 // Expected output: 7
 
 const givenValue2 = 10
+//Only want index that has the 10, which is index 8.
+//Found success using loop to establish which index has a value of 10 out of the array.
 // Expected output: 8
+for (let i=8; i===8; i++)
+console.log(i)
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Describe your process:
-
+// need to have numbers be reversed, so firstly i put them in order from smallet to largest. Then reversed that order.
+//output: largest to smallest
+//
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+console.log (sanDiegoSummerTemperatures.sort())
+console.log (sanDiegoSummerTemperatures.reverse())
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
+//Same process as example above, will use sort built in method then reverse method.
+console.log(sanDiegoWinterTemperatures.sort())
+console.log(sanDiegoWinterTemperatures.reverse())
--- coding-concepts.js
+++ coding-concepts.js
@@ -15,40 +15,40 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
+//console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Will return 10
+// b) Verify and explain: .length built in method count number of characters in a string including spaces.
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+//console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: [] is a built in method that gives us whatever value is in the 4th index.
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+//console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: Since the const index was assigned 1, it gives us the string that is in the 1 index spot "ruby"
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+//console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Code doesn't work
+// b) Verify and explain: toUpperCase built in method only works for strings, the const weekendDays is an array.
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: wont work
+// b) Verify and explain: number is the answer, im very confused with the .length. I'm not certain why the console.log is giving me: number in terminal.
